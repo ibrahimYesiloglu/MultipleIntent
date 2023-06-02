bcabc398891191922c7b953f56b0ed8d01e72067
ibrahimYesiloglu <cizgi.2d@gmail.com>
2023-06-02 20:25:02 +0200

    Erste aufgabe beschäftigt.

--- app/src/main/java/com/example/multipleintent/MainActivity.kt
+++ app/src/main/java/com/example/multipleintent/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.multipleintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+    val count: Int =0
+    lateinit var sayHelloButton: Button
+    lateinit var countButton: Button
+    lateinit var countText: TextView
+
+
+
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        sayHelloButton= findViewById(R.id.button_say_hello)
+        countButton= findViewById(R.id.button_count)
+        countText= findViewById(R.id.textView_number)
+
+
+
+        sayHelloButton.setOnClickListener {View.OnClickListener {
+                openSecondActivity()
+            }
+        countButton.setOnClickListener { View.OnClickListener {
+            plusCount()
+        } }
+        }
+
+
+    }
+    fun openSecondActivity() {
+    }
+    fun plusCount() {
+
     }
 }
---
+++ app/src/main/java/com/example/multipleintent/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.example.multipleintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+
+
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
